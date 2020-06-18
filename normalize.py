aba039f9bf129cf8874d94bc55ee9a538c892f28
danilkastar440 <48782507+danilkastar440@users.noreply.github.com>
2020-06-18 08:46:31 -0400

    Add files via upload

--- normalize.py
+++ normalize.py
@@ -32,16 +32,19 @@ def write_data_mysql(data):
     cursor_mysql.execute('drop table if exists transmissions')
     cursor_mysql.execute('drop table if exists brands')
     cursor_mysql.execute('drop table if exists wheels')
+    #create tables
     cursor_mysql.execute('create table cars(model varchar(255), brand varchar(255),engine varchar(255), transmission varchar(255), price integer(255),wheel varchar(255))')
     cursor_mysql.execute('create table engines(engine_model varchar(255),engine_power integer(255),engine_volume integer(255), engine_type varchar(255))')
     cursor_mysql.execute('create table transmissions(transmission_model varchar(255),transmission_type varchar(255),transmission_gears_number integer(255))')
     cursor_mysql.execute('create table brands(brand_name varchar(255), brand_creator_country varchar(255))')
     cursor_mysql.execute('create table wheels(wheel_model varchar(255), wheel_radius integer(255), wheel_color varchar(255))')
-    cursor_mysql.execute('ALTER TABLE cars add primary key(model)')
-    cursor_mysql.execute('ALTER TABLE engines add primary key(engine_model)')
-    cursor_mysql.execute('ALTER TABLE transmissions add primary key(transmission_model)')
-    cursor_mysql.execute('ALTER TABLE brands add primary key(brand_name)')
-    cursor_mysql.execute('ALTER TABLE wheels add primary key(wheel_model)')
+    #add primary keys
+    cursor_mysql.execute('alter table cars add primary key(model)')
+    cursor_mysql.execute('alter table engines add primary key(engine_model)')
+    cursor_mysql.execute('alter table transmissions add primary key(transmission_model)')
+    cursor_mysql.execute('alter table brands add primary key(brand_name)')
+    cursor_mysql.execute('alter table wheels add primary key(wheel_model)')
+    #add connections
     for item in data:
         #print(item)
         #('2114', 'LADA', 'Russia', 'V123', '80', '16', 'L4', 'M123', 'M', '5', 'Best kolesa', '13', 'White', '100000')
@@ -50,7 +53,11 @@ def write_data_mysql(data):
         cursor_mysql.execute(f'INSERT INTO transmissions(transmission_model,transmission_type,transmission_gears_number) values ("{item[7]}","{item[8]}",{item[9]}) ON DUPLICATE KEY UPDATE transmission_model = "{item[7]}"')
         cursor_mysql.execute(f'INSERT INTO brands(brand_name,brand_creator_country) values ("{item[1]}","{item[2]}") ON DUPLICATE KEY UPDATE brand_name = "{item[1]}"') 
         cursor_mysql.execute(f'INSERT INTO wheels(wheel_model,wheel_radius,wheel_color) values ("{item[10]}",{item[11]},"{item[12]}") ON DUPLICATE KEY UPDATE wheel_model = "{item[10]}"') 
-        conn_mysql.commit()
+    cursor_mysql.execute('alter table cars add constraint cars_brand_fk foreign key (brand) references brands (brand_name)')
+    cursor_mysql.execute('alter table cars add constraint cars_engine_fk foreign key (engine) references engines (engine_model)')
+    cursor_mysql.execute('alter table cars add constraint cars_transmission_fk foreign key (transmission) references transmissions (transmission_model)')
+    cursor_mysql.execute('alter table cars add constraint cars_wheel_fk foreign key (wheel) references wheels (wheel_model)')
+    conn_mysql.commit()
 
     conn_mysql.close()
     print(f"Successfully inserted to univer ")
@@ -110,11 +117,15 @@ def export():
 
 
 
-if args.export:
+if args.export and args.filename:
+    data = read_data_sqlite3(args.filename)
+    write_data_mysql(data)
     export()
 elif args.filename:
     data = read_data_sqlite3(args.filename)
     write_data_mysql(data)
+elif args.export():
+    export()
 else:
     print("Add some functions or --help for help")
 
